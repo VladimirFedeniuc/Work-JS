fd9ee874218adc120acdd6f97c90606ec3ce25ae
VladimirFedeniuc <vladimir.fedeniuc.gmail.com>
2022-10-14 08:20:48 +0300

    first commit

---
+++ js/interpolation.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const category = "toys";
+
+// console.log("https://someurl.com/" + category + "/" + "4"); // неудобный вариант.
+
+console.log(`https://someurl.com/${category}/5`); // интерполяция при помощи косых бэктик `` и ${переменная}
+
+const user = "Ivan";
+alert(`Привет, ${user}`);
